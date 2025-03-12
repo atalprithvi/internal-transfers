2d6642dc0ae1ff9d5eb26196e0c1ca4d693169cf
atalprithvi <prithvi.atal@gmail.com>
2025-03-13 00:53:20 +0530

    Add files via upload

---
+++ tests/integration/test_account_it.go
@@ -0,0 +1,16 @@
+package integration
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAccountIntegration(t *testing.T) {
+	resp, err := http.Get("http://localhost:8080/api/v1/accounts/12")
+	if err != nil {
+		t.Fatalf("Failed to send GET request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	t.Logf("Status Code: %d", resp.StatusCode)
+}
