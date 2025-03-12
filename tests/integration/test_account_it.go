af559f30fd5ad57839931ff906606f9878f2b8b6
atalprithvi <prithvi.atal@gmail.com>
2025-03-13 01:12:56 +0530

    Update test_account_it.go

--- tests/integration/test_account_it.go
+++ tests/integration/test_account_it.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 	"testing"
 )
-
+//TODO complete it
 func TestGetAccountIntegration(t *testing.T) {
 	resp, err := http.Get("http://localhost:8080/api/v1/accounts/12")
 	if err != nil {
